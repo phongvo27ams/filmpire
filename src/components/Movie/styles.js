bac4408229b7475448dfec62a1da6799aff5c0bc
phongvo27ams <phongvo27ams@gmail.com>
2023-03-06 15:40:16 +0700

    Finish the movie view

--- src/components/Movie/styles.js
+++ src/components/Movie/styles.js
@@ -4,10 +4,33 @@ export default makeStyles((theme) => ({
     movie: {
         padding: '10px'
     },
+    links: {
+        alignItems: 'center',
+        fontWeight: 'bolder',
+        textDecoration: 'none',
+        [theme.breakpoints.up('xs')]: {
+            display: 'flex',
+            flexDirection: 'column'
+        },
+        '&:hover': {
+            cursor: 'pointer',
+        }
+    },
+    image: {
+        borderRadius: '20px',
+        height: '300px',
+        width: '100%',
+        height: 'auto',
+        marginBottom: '10px',
+        transition: '300ms',
+        '&:hover': {
+            transform: 'scale(1.05)'
+        }
+    },
     title: {
         color: theme.palette.text.primary,
         textOverflow: 'ellipsis',
-        width: '230px',
+        width: '100%',
         whiteSpace: 'nowrap',
         overflow: 'hidden',
         marginTop: '10px',
